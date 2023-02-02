ef9e45e35a392ff328fbdcd703c9e925f37b7aec
LeaderbotX400 <eric@singerfamily.ca>
2023-02-02 11:24:11 -0700

    search algorithms

--- binary-search.js
+++ binary-search.js
@@ -1,5 +1,10 @@
+/**
+ * @param {number[]} array
+ * @param {number} searchTerm
+ */
+
 function binarySearch(array, searchTerm) {
-	/*
+  /*
 	 ** Check the middle element, if it's higher than the searchTerm
 	 ** then check the element in between the start and the middle, otherwise
 	 ** check the element between the middle and the end
@@ -8,7 +13,24 @@ function binarySearch(array, searchTerm) {
 	 ** keep repeating with smaller and smaller subsets until you find the searchTerm
 	 */
 
-	return null;
+  let start = 0;
+  let end = array.length - 1;
+
+  while (start <= end) {
+    let middle = Math.floor((start + end) / 2);
+
+    if (array[middle] === searchTerm) {
+      return middle;
+    }
+
+    if (array[middle] < searchTerm) {
+      start = middle + 1;
+    } else {
+      end = middle - 1;
+    }
+  }
+
+  return -1;
 }
 
 module.exports = binarySearch;
--- linear-search.js
+++ linear-search.js
@@ -1,12 +1,25 @@
-function linearSearch(array, searchTerm) {
-	/*
-	 ** Check each element from start to finish
-	 ** until you find an element that matches the search term
-	 **
-	 ** Return the index of the found element
-	 */
+/**
+ * @param {number[]} array
+ * @param {number} searchTerm
+ */
 
-	return null;
+function linearSearch(array, searchTerm) {
+  /*
+   ** Check each element from start to finish
+   ** until you find an element that matches the search term
+   **
+   ** Return the index of the found element
+   */
+
+  let index = 0;
+
+  array.forEach((num, i) => {
+    if (num == searchTerm) {
+      index = i;
+    }
+  });
+
+  return index;
 }
 
 module.exports = linearSearch;
